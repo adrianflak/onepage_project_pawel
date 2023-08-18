393982c475dacbf99b9ab614857e10a24f901bc2
Paweł <p.skiba1995@gmail.com>
2023-08-18 09:23:37 +0200

    New changes

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function mojaFunkcja() {
+    let x = "Moja Funkcja"
+    document.getElementById("mojeid").innerHTML = x;
+} 
+function mojaFunkcja2() {
+    document.write("Witaj świecie")
+} 
+mojaFunkcja2();
